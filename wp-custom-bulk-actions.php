da0a105f1322cc1e1848fb5bed56e6f6c41af944
Dinesh Kesarwani <dinesh.cyberwani@gmail.com>
2015-08-05 17:38:23 +0530

    Typo fixed.

--- wp-custom-bulk-actions.php
+++ wp-custom-bulk-actions.php
@@ -163,7 +163,7 @@ if (!class_exists('Seravo_Custom_Bulk_Action')) {
 				$sendback = remove_query_arg( array('action', 'paged', 'mode', 'action2', 'tags_input', 'post_author', 'comment_status', 'ping_status', '_status',  'post', 'bulk_edit', 'post_view'), $sendback );
 				
 				wp_redirect($sendback);
-				exit();di
+				exit();
 			}
 		}
 		
